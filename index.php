12fdec4311c15ad9cf67cc36278d14a9c98aef5d
thekill8593 <thekill8593@hotmail.com>
2019-10-24 17:49:35 -0500

    timeline v1

---
+++ index.php
@@ -0,0 +1,55 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Timeline</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/tailwind.min.css">
+    <link rel="stylesheet" type="text/css" href="css/fontawesome.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<div class="bg-gray-500 w-full h-screen flex justify-center items-center flex-col">
+		<div class="m-auto  relative overflow-hidden" id="timeline-container">
+            <div id="btn-right" class="w-8 h-8 rounded-full absolute cursor-pointer shadow-lg" style="top: calc(50% - 20px); z-index: 50;">
+                <i class="fas fa-arrow-circle-left text-gray-400 fa-2x hover:text-gray-600"></i>
+            </div>
+            <div id="btn-left" class="w-8 h-8 rounded-full absolute cursor-pointer shadow-lg" style="top: calc(50% - 20px); right: 0; z-index: 50;">
+                <i class="fas fa-arrow-circle-right text-gray-400 fa-2x hover:text-gray-600"></i>
+            </div>
+			<div class="timeline flex" id="timeline">
+                <?php for($i = 0; $i < 10; $i++): ?>
+				    <div class="flex flex-col items-center time-line-item">
+                        <div class="relative z-50 mt-2 w-20 h-20 rounded-full bg-red-500 flex items-center justify-center">
+                            <i class="fas fa-handshake fa-3x text-white"></i>
+                        </div>
+                        <div class="colored-box relative flex items-center justify-center mt-20 bg-red-100 w-full h-12">
+                            <div class="circle w-3 h-3 bg-transparent rounded-full border-2 border-black"></div>
+                        </div>
+                        <div class="mb-2 flex flex-col ">
+                            <span class="text-center text-4xl font-bold text-white">201<?php echo $i ?></span>
+                            <span class="text-center text-white text-xs">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem, molestiae.</span>
+                        </div>
+				    </div>
+                    <div class="inverted flex flex-col items-center justify-end time-line-item" >
+                        <div class="mb-2 flex flex-col" style="margin-top: 53px;">
+                            <span class="text-center text-4xl font-bold text-white">201<?php echo $i ?></span>
+                            <span class="text-center text-white text-xs">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem, molestiae.</span>
+                        </div>
+                        <div class="colored-box-inverted relative flex items-center justify-center mb-6 bg-orange-500 w-full h-12">
+                            <div class="circle w-3 h-3 bg-transparent rounded-full border-2 border-black"></div>
+                        </div>
+                        <div class="relative z-50 mt-16 w-20 h-20 rounded-full bg-orange-500 flex items-center justify-center">
+                            <i class="fas fa-cart-plus fa-3x text-white"></i>
+                        </div>
+                    </div>
+                <?php endfor; ?>
+			</div>
+		</div>
+	</div>
+
+
+	<script type="text/javascript" src="timeline/timeline.js"></script>
+</body>
+</html>
---
+++ timeline/timeline.js
@@ -0,0 +1,141 @@
+class Timeline {
+    constructor (options = {'itemsPerPage': 5}) {
+        //initialize constructorParams
+        this.options = options;
+
+        //check screen size and change
+        this.screenWidth = Math.max(document.documentElement.clientWidth, screen.width || 0);
+        this.itemWidth = this.calculateItemWidth();
+        this.options.itemsPerPage = this.calculateItemsPerPage();
+        this.itemsShowed = this.options.itemsPerPage;
+
+        this.btnGoLeft = document.getElementById('btn-left');
+        this.btnGoRight = document.getElementById('btn-right');
+        this.timeline = document.getElementById('timeline');
+        this.timelineContainer = document.getElementById('timeline-container');
+        this.items = this.timeline.children;
+        this.timelineWidth = this.calculateAndSetWidthTimeline();
+        this.timelineContainerWidth = this.calculateAndSetWidthTimelineContainer();
+        this.setWidthToItems();
+
+        this.isAnimating = false;
+
+        this.btnGoLeft.addEventListener('click', () => {
+            this.moveTimeline();
+        });
+
+        this.btnGoRight.addEventListener('click', () => {
+            this.moveTimeline("right");
+        });
+
+        this.drawButtons();
+    }
+
+    calculateAndSetWidthTimeline() {
+        const timelineWidth = this.items.length * this.itemWidth;
+        this.timeline.style.width = `${timelineWidth}px`;
+        return timelineWidth;
+    }
+
+    moveTimeline(dir = "left") {
+        if (this.isAnimating) return;
+
+        let currentMargin = this.getNumericValue(this.timeline.style.marginLeft);
+        if ((currentMargin === 0 && dir === "right" )|| (Math.abs(currentMargin) >= (this.timelineWidth - this.timelineContainerWidth) && dir === "left")) {
+            this.isAnimating = false;
+            return;
+        }
+
+        this.isAnimating = true;
+        const numberOfItemsToShow = this.numberOfItemsToShow(dir);
+        const val = dir === "left" ? -(numberOfItemsToShow*this.itemWidth) : (numberOfItemsToShow*this.itemWidth);
+        const targetMargin = currentMargin + val;
+
+        if (dir === "left") {
+            this.itemsShowed += numberOfItemsToShow;
+        } else if (dir === "right") {
+            this.itemsShowed -= numberOfItemsToShow;
+        }
+
+        this.drawButtons();
+
+        const animation = setInterval(() => {
+            if (currentMargin === targetMargin) {
+                this.isAnimating = false;
+                clearInterval(animation);
+                return;
+            }
+            currentMargin = currentMargin > targetMargin ? currentMargin-=10 : currentMargin+=10;
+            this.timeline.style.marginLeft = `${currentMargin}px`;
+        },1);
+    }
+
+    numberOfItemsToShow(dir) {
+        if (dir === 'left') {
+            return this.items.length - this.itemsShowed > 1 ?  2 :  1;
+        } else if (dir === 'right') {
+            return this.itemsShowed - this.options.itemsPerPage > 1 ? 2 : 1;
+        }
+    }
+
+    getNumericValue(value, units = "px") {
+        try {
+            if (!value) return 0;
+            return parseInt(value.replace(units, ""));
+        }catch (e) {
+            console.error(e);
+        }
+        return 0;
+    }
+
+    calculateAndSetWidthTimelineContainer() {
+        const timeLineContainerWidth = this.options.itemsPerPage * this.itemWidth;
+        this.timelineContainer.style.width = `${timeLineContainerWidth}px`;
+        return timeLineContainerWidth;
+    }
+
+    setWidthToItems() {
+        for (let i = 0; i < this.items.length; i++) {
+            this.items[i].style.width = `${this.itemWidth}px`;
+        }
+    }
+
+    calculateItemWidth() {
+        return this.screenWidth >= 500 ? 180 : 160;
+    }
+
+    drawButtons () {
+        if (this.itemsShowed === this.options.itemsPerPage) {
+            this.btnGoRight.style.display = 'none';
+        } else {
+            this.btnGoRight.style.display = 'block';
+        }
+
+        if (this.itemsShowed === this.items.length) {
+            this.btnGoLeft.style.display = 'none';
+        } else {
+            this.btnGoLeft.style.display = 'block';
+        }
+    }
+
+    calculateItemsPerPage () {
+        if (this.screenWidth > 1240) {
+            return this.options.itemsPerPage;
+        }else if (this.screenWidth > 1100) {
+            return 5;
+        }else if (this.screenWidth > 850) {
+            return 4;
+        } else if (this.screenWidth > 650) {
+            return 3;
+        } else {
+            return 2;
+        }
+    }
+}
+
+window.onload = function () {
+    const timeline = new Timeline({
+        itemsPerPage: 6
+    });
+};
+
